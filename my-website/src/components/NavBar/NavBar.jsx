9fd64e92990ab467dada59dd270b8ef2c7996683
daniellebandong <46451872+daniellebandong@users.noreply.github.com>
2022-11-02 14:33:49 -0600

    installed hamburger library

--- my-website/src/components/NavBar/NavBar.jsx
+++ my-website/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+
+import Hamburger from 'hamburger-react'
+
 import './navBarStyles.scss'
 
-
+function toggleMenu(){
+    
+}
 const NavBar = () => {
     return ( 
         <header>
             {/* should probably add a logo here and make it clickable */}
-            <nav>
+            <nav className='mainNav'>
                 <ul>
                     <li><a href="">About Me</a></li>
                     <li><a href="">My Work</a></li>
